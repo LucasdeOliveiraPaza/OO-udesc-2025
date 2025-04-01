c726961f795706a5defd626d1f73cd5de74e9ff4
lucas <lucasdeoliveirapaza@gmail.com>
2025-03-31 21:14:04 -0300

    segundo commmit

---
+++ src/OlaMundo.java
@@ -0,0 +1,12 @@
+public class OlaMundo {
+    String nome = "UDESC";
+    Integer idade = 42;
+
+    void imprimirDados() {
+        System.out.println(nome + " - " + idade);
+    }
+
+    void imprimir(String nome, int idade) {
+        System.out.println(nome + " - " + idade);
+    }
+}
---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,7 @@
+public class TestaOlaMundo {
+    public static void main(String[] args) {
+        OlaMundo mundo = new OlaMundo();
+
+        mundo.imprimir("Lucas", 21);
+    }
+}
