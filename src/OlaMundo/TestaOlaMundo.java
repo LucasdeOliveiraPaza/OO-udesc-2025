00cad36200a751cd2afd6e354c6e0ed4b0fcb05d
lucas <lucasdeoliveirapaza@gmail.com>
2025-03-31 21:44:18 -0300

    adicionar scanner

--- src/OlaMundo/TestaOlaMundo.java
+++ src/OlaMundo/TestaOlaMundo.java
@@ -1,8 +1,18 @@
 package OlaMundo;
 
+import java.util.Scanner;
+
 public class TestaOlaMundo {
     public static void main(String[] args) {
         OlaMundo mundo = new OlaMundo();
-        mundo.imprimirDados();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scan.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scan.nextInt();
+
+        mundo.imprimir(nome, idade);
     }
 }
